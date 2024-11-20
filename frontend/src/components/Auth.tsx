8c23c193cb5c9215c0bc8d79f33937a724c2d257
AaraV <aaravrawat00000@gmail.com>
2024-11-21 00:00:00 +0530

    now fixed

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -90,12 +90,12 @@ interface LabelledInputType {
     type?: string;
 }
 
-function LabelledInput({ label, placeholder, onChange, type }: LabelledInputType) {
+function LabelledInput({ label, placeholder, onChange, type, value }: LabelledInputType) {
     return <div>
        
         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{label}</label>
 
-        <input onChange={onChange} type={type || "text"}  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={placeholder} required />
+        <input onChange={onChange} type={type || "text"}  value={value}className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={placeholder} required />
 
     </div>
 }
